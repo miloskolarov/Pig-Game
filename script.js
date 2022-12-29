67d25b6721f7083902bec7ef9797477f90a2689c
miloskolarov <milos.v.kolarov@gmail.com>
2022-12-29 16:49:10 +0100

    Add initial state function and call it by pressin 'New game'

--- script.js
+++ script.js
@@ -14,14 +14,31 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
 // Set initial state:
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let currentScore, activePlayer, playing, scores;
+
+const init = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -73,7 +90,6 @@ btnHold.addEventListener("click", function () {
       switchPlayer();
     }
   }
-
-  // finish the hame
-  // if not, switch player
 });
+
+btnNew.addEventListener("click", init);

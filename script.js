ed733a71169e73ed65136098fc9076ccfcd68536
miloskolarov <milos.v.kolarov@gmail.com>
2022-12-29 14:48:30 +0100

    Reset the score on player change

--- script.js
+++ script.js
@@ -16,7 +16,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Roll the die
 btnRoll.addEventListener("click", function () {
@@ -30,8 +32,12 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     // Add to score
     currentScore = currentScore + dice;
-    current0El.textContent = currentScore; /* CHANGE!*/
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch to the next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
   }
 });
